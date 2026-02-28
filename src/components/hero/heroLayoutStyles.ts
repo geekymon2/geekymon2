dc059a1a100604dbd41aaf2b6e89840b43c81cab
Geeky Mon2 <geekymon2@gmail.com>
2026-02-28 21:15:22 +1100

    fixed hero

--- src/components/hero/heroLayoutStyles.ts
+++ src/components/hero/heroLayoutStyles.ts
@@ -28,3 +28,38 @@ export const Background = styled("div")({
 export interface HeroLayoutProps {
   sxBackground: SxProps<Theme>;
 }
+
+export const HeroLayoutRoot = styled("section")<{ theme?: Theme }>((props) => ({
+  color: props.theme?.palette.common.white,
+  position: "relative",
+  display: "flex",
+  alignItems: "center",
+  [`@media (min-width: 600px)`]: {
+    height: "80vh",
+    minHeight: 500,
+    maxHeight: 1300,
+  },
+}));
+
+export const boxStyle: CSSProperties = {
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: "rgba(0, 0, 0, 0.5)",
+  zIndex: -1,
+};
+
+export const containerStyle: CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+};
+
+export const arrowBoxStyle: CSSProperties = {
+  height: "16px",
+  width: "12px",
+  position: "absolute",
+  bottom: 32,
+};
