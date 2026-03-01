9a851491f2e3464973aed1516c78d1963546a07f
Geeky Mon2 <geekymon2@gmail.com>
2026-03-01 21:25:28 +1100

    updates to hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -2,25 +2,17 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import HeroLayout from "./HeroLayout";
 
-const backgroundImage =
-  "https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400";
+const backgroundImage = "/coding.png?auto=format&fit=crop&w=1440";
 
 export default function ProductHero() {
   return (
     <HeroLayout
       sxBackground={{
         backgroundImage: `url(${backgroundImage})`,
-        backgroundColor: "#7fc7d9", // Average color of the background image.
-        backgroundPosition: "center",
       }}
     >
-      <img
-        style={{ display: "none" }}
-        src={backgroundImage}
-        alt="increase priority"
-      />
       <Typography color="inherit" align="center" variant="h2">
-        Upgrade your Sundays
+        Engineer. Problem Solver. Creator.
       </Typography>
       <Typography
         color="inherit"
@@ -28,21 +20,17 @@ export default function ProductHero() {
         variant="h5"
         sx={{ mb: 4, mt: { xs: 4, sm: 10 } }}
       >
-        Enjoy secret offers up to -70% off the best luxury hotels every Sunday.
+        Turning ideas into user-friendly, high performance products.
       </Typography>
       <Button
         color="secondary"
         variant="contained"
         size="large"
         component="a"
-        href="/premium-themes/onepirate/sign-up/"
         sx={{ minWidth: 200 }}
       >
-        Register
+        My Skills
       </Button>
-      <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
-        Discover the experience
-      </Typography>
     </HeroLayout>
   );
 }
