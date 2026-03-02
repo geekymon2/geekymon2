8cb02cdfce34e2c840bc7f65c0fba2d694ce1f73
Geeky Mon2 <geekymon2@gmail.com>
2026-03-02 20:27:47 +1100

    added react hooks linting

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,17 @@ import js from "@eslint/js";
 import ts from "typescript-eslint";
 import react from "eslint-plugin-react";
 import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
 
 export default [
   js.configs.recommended,
-  ...ts.configs.recommended,
+  ...ts.configs.recommendedTypeChecked,
   {
     files: ["src/**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
       parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname,
         ecmaVersion: "latest",
         sourceType: "module",
       },
@@ -19,9 +22,12 @@ export default [
     },
     plugins: {
       react,
+      "react-hooks": reactHooks,
     },
     rules: {
       "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
 ];
