123b4b816deb9f26dce52e266014f949dd497c2a
Geeky Mon2 <geekymon2@gmail.com>
2026-03-02 21:23:35 +1100

    more eslinbt changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,7 +24,7 @@ export default [
     files: ["src/**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
       parserOptions: {
-        project: "./tsconfig.eslint.json",
+        project: new URL("./tsconfig.eslint.json", import.meta.url).pathname,
         tsconfigRootDir: import.meta.dirname,
         ecmaVersion: "latest",
         sourceType: "module",
